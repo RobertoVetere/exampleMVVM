cc697d5a237c299747d01cd59011884f6761c0ec
Roberto Vetere <Roberto.vetere@gmail.com>
2024-06-10 18:08:12 +0200

    ADD Quote Model + QuoteProvider

---
+++ app/src/main/java/com/holidevs/ejemplomvvm/model/QuoteModel.kt
@@ -0,0 +1,3 @@
+package com.holidevs.ejemplomvvm.model
+
+data class QuoteModel(val quote:String, val author:String)
---
+++ app/src/main/java/com/holidevs/ejemplomvvm/model/QuoteProvider.kt
@@ -0,0 +1,26 @@
+package com.holidevs.ejemplomvvm.model
+
+class QuoteProvider {
+    val quotes = listOf(
+        QuoteModel("La vida sin examen no vale la pena ser vivida.", "Sócrates"),
+        QuoteModel("Pienso, luego existo.", "René Descartes"),
+        QuoteModel("El hombre es la medida de todas las cosas.", "Protágoras"),
+        QuoteModel("Dios ha muerto.", "Friedrich Nietzsche"),
+        QuoteModel("Conócete a ti mismo.", "Sócrates"),
+        QuoteModel("Solo sé que no sé nada.", "Sócrates"),
+        QuoteModel("La felicidad es el significado y el propósito de la vida, el objetivo y fin de la existencia humana.", "Aristóteles"),
+        QuoteModel("El conocimiento es poder.", "Francis Bacon"),
+        QuoteModel("El hombre está condenado a ser libre.", "Jean-Paul Sartre"),
+        QuoteModel("No puedo enseñar nada a nadie. Solo puedo hacerles pensar.", "Sócrates"),
+        QuoteModel("El alma razonable debe gobernar el cuerpo.", "Platón"),
+        QuoteModel("El hombre es un lobo para el hombre.", "Thomas Hobbes"),
+        QuoteModel("La libertad es la obediencia a la ley que uno mismo se ha trazado.", "Jean-Jacques Rousseau"),
+        QuoteModel("La religión es el opio del pueblo.", "Karl Marx"),
+        QuoteModel("La única cosa que sé es que nada sé.", "Sócrates"),
+        QuoteModel("La filosofía es la ciencia que complica las cosas que todo el mundo sabe.", "Juan Benet"),
+        QuoteModel("El hombre es un ser para la muerte.", "Martin Heidegger"),
+        QuoteModel("Lo que no me mata, me hace más fuerte.", "Friedrich Nietzsche"),
+        QuoteModel("La vida debe ser comprendida hacia atrás. Pero debe ser vivida hacia adelante.", "Søren Kierkegaard"),
+        QuoteModel("La imaginación es más importante que el conocimiento.", "Albert Einstein")
+    )
+}
